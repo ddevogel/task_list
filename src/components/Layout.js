d922569f1340485aab2f953164f86a1e1387cd74
Calvin D <cdevogel25@gmail.com>
2017-03-21 17:17:44 -0500

    added actions

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {addTask, removeTask, completeTask, init} from '../actions/taskActions'
 
 @connect((store) => {
         return {
@@ -11,10 +12,19 @@ import {connect} from 'react-redux'
 
 export default class Layout extends React.Component {
     componentWillMount() {
-        this.props.dispatch({type:'INIT', payload: [0,'Calvin']})
+        this.props.dispatch(init([0, 'Calvin']))
     }
 
     render() {
         const {user, tasks, finished} = this.props
+
+        const mappedTasks = tasks.user.tasks.map(task => <li>{task}</li>)
+        const mappedFinished = tasks.user.finished.map(finished => <li>{finished}</li>)
+
+        return <div>
+            <h1>{user.name}</h1>
+            <ol>{mappedTasks}</ol>
+            <ol>{mappedFinished}</ol>
+        </div>
     }
 }
